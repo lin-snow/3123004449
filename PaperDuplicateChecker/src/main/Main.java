80490a2ec9133c0ac2147e6515b55c8d9fcc1404
L1nSn0w <l1nsn0w@qq.com>
2025-03-03 21:47:57 +0800

    fix: remove unused exception

--- PaperDuplicateChecker/src/main/Main.java
+++ PaperDuplicateChecker/src/main/Main.java
@@ -20,31 +20,24 @@ public class Main {
         String plagiarizedFilePath = args[1];
         String answerFilePath = args[2];
 
-        try {
-            // 读取原始文件内容
-            String originalContent = FileUtil.readFile(originalFilePath);
-            System.out.println("对比文本1： " + originalContent);
+        // 读取原始文件内容
+        String originalContent = FileUtil.readFile(originalFilePath);
+        System.out.println("对比文本1： " + originalContent);
 
-            // 读取抄袭文件内容
-            String plagiarizedContent = FileUtil.readFile(plagiarizedFilePath);
-            System.out.println("对比文本2： " + plagiarizedContent);
+        // 读取抄袭文件内容
+        String plagiarizedContent = FileUtil.readFile(plagiarizedFilePath);
+        System.out.println("对比文本2： " + plagiarizedContent);
 
-            BigInteger hash1 = SimHashUtils.generateSimHash(originalContent);
-            BigInteger hash2 = SimHashUtils.generateSimHash(plagiarizedContent);
+        BigInteger hash1 = SimHashUtils.generateSimHash(originalContent);
+        BigInteger hash2 = SimHashUtils.generateSimHash(plagiarizedContent);
 
+//        System.out.println(hash1);
+//        System.out.println(hash2);
+        double similarity = SimHashUtils.getSimilarity(hash1, hash2);
+        String result = String.format("%.2f", similarity * 0.4 + SimHashUtils.getJaccardSimilarity(originalContent, plagiarizedContent) * 0.6);
+        System.out.printf("相似度：" + result);
 
-//            System.out.println(hash1);
-//            System.out.println(hash2);
-            double similarity = SimHashUtils.getSimilarity(hash1, hash2);
-            String result = String.format("%.2f", similarity * 0.4 + SimHashUtils.getJaccardSimilarity(originalContent, plagiarizedContent) * 0.6);
-            System.out.printf("相似度：" + result);
-
-            // 将结果写入文件
-            FileUtil.writeFile(answerFilePath, result);
-
-
-        } catch (FileNotFoundException e) {
-            System.out.println("找不到文件: " + e.getMessage());
-        }
+        // 将结果写入文件
+        FileUtil.writeFile(answerFilePath, result);
     }
 }
